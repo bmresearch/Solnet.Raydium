a27c422a5a44c9302e45415ea0214dc02776333d
Tim Hastings <mliquizard@gmail.com>
2021-08-19 15:01:46 +0100

    Buffer decoding

---
+++ Solnet.Raydium.Test/DecodingTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solnet.Raydium.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solnet.Raydium.Test
+{
+    [TestClass]
+    public class DecodingTests
+    {
+
+        [TestMethod]
+        public void TestBufferDecoder()
+        {
+            // 0x01 + 0x00000000 00000000
+            var buf = BufferDecoder.CreateFromBase58("jpXCZedGfVR");
+            Assert.AreEqual(1, buf.ReadByte());
+            Assert.AreEqual(0U, buf.ReadU64());
+        }
+
+        [TestMethod, ExpectedException(typeof(ApplicationException))]
+        public void TestBufferByteExhausted()
+        {
+            var buf = BufferDecoder.CreateFromBase58("jpXCZedGfVR");
+            Assert.AreEqual(1, buf.ReadByte());
+            Assert.AreEqual(0U, buf.ReadU64());
+            buf.ReadByte();
+        }
+
+        [TestMethod, ExpectedException(typeof(ApplicationException))]
+        public void TestBufferU64Exhausted()
+        {
+            var buf = BufferDecoder.CreateFromBase58("jpXCZedGfVR");
+            Assert.AreEqual(1, buf.ReadByte());
+            Assert.AreEqual(0, buf.ReadByte());
+            buf.ReadU64();
+        }
+
+    }
+}
---
+++ Solnet.Raydium/Utilities/BufferDecoder.cs
@@ -0,0 +1,139 @@
+﻿using Solnet.Wallet;
+using Solnet.Wallet.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solnet.Raydium.Utilities
+{
+    /// <summary>
+    /// Utility class for decoding byte buffers
+    /// </summary>
+    public class BufferDecoder
+    {
+
+        /// <summary>
+        /// Length of the byte buffer.
+        /// </summary>
+        private int Length { get; }
+
+        /// <summary>
+        /// Current read position.
+        /// </summary>
+        public int Cursor { get; set; }
+
+        /// <summary>
+        /// The internal buffer.
+        /// </summary>
+        private byte[] _buffer;
+
+        /// <summary>
+        /// Constructs a buffer decode with the give byte buffer.
+        /// </summary>
+        /// <param name="buffer"></param>
+        public BufferDecoder(byte[] buffer) 
+        {
+            _buffer = buffer ?? throw new ArgumentNullException(nameof(buffer));
+            Cursor = 0;
+            Length = buffer.Length;
+        }
+
+        /// <summary>
+        /// Decodes a Base64 data string into bytes and returns a BufferDecoder initialized with the decoded buffer.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>An instance of the BufferDecoder initialized with the decoded base 64 string.</returns>
+        public static BufferDecoder CreateFromBase64(string data)
+        {
+            byte[] buffer = Convert.FromBase64String(data ?? throw new ArgumentNullException(nameof(data)));
+            return new BufferDecoder(buffer);
+        }
+
+        /// <summary>
+        /// Decodes a Base58 data string into bytes and returns a BufferDecoder initialized with the decoded buffer.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>An instance of the BufferDecoder initialized with the decoded base 58 string.</returns>
+        public static BufferDecoder CreateFromBase58(string data)
+        {
+            var enc = new Base58Encoder();
+            byte[] buffer = enc.DecodeData(data ?? throw new ArgumentNullException(nameof(data)));
+            return new BufferDecoder(buffer);
+        }
+
+        /// <summary>
+        /// Reads one byte from the buffer and advances the cursor position.
+        /// </summary>
+        /// <returns>The byte read.</returns>
+        public byte ReadByte()
+        {
+            if (Cursor >= Length) throw new ApplicationException("Buffer exhausted");
+            byte value = _buffer[Cursor];
+            Cursor += 1;
+            return value;
+        }
+
+        /// <summary>
+        /// Reads one byte from the buffer and advances the cursor position.
+        /// </summary>
+        /// <returns>The byte read.</returns>
+        public Span<byte> ReadBytes(int bytesToRead)
+        {
+            if ((Cursor+bytesToRead) > Length) throw new ApplicationException("Buffer exhausted");
+            var span = _buffer.AsSpan(Cursor, bytesToRead);
+            Cursor += bytesToRead;
+            return span;
+        }
+
+        /// <summary>
+        /// Reads a PublicKey from the buffer and advances the cursor position.
+        /// </summary>
+        /// <returns>A PublicKey instance.</returns>
+        public PublicKey ReadPublicKey()
+        {
+            if ((Cursor + 32) > Length) throw new ApplicationException("Buffer exhausted");
+            var pubkey = new PublicKey(_buffer.AsSpan(Cursor, 32));
+            Cursor += 32;
+            return pubkey;
+        }
+
+        /// <summary>
+        /// Reads a uint from the buffer and advances the cursor position.
+        /// </summary>
+        /// <returns>A uint value read from the buffer.</returns>
+        public uint ReadU32()
+        {
+            if ((Cursor + 4) > Length) throw new ApplicationException("Buffer exhausted");
+            var value = BitConverter.ToUInt32(_buffer, Cursor);
+            Cursor += 4;
+            return value;
+        }
+
+        /// <summary>
+        /// Reads a ulong from the buffer and advances the cursor position.
+        /// </summary>
+        /// <returns>An ulong value read from the buffer.</returns>
+        public ulong ReadU64()
+        {
+            if ((Cursor + 8) > Length) throw new ApplicationException("Buffer exhausted");
+            var value = BitConverter.ToUInt64(_buffer, Cursor);
+            Cursor += 8;
+            return value;
+        }
+
+        /// <summary>
+        /// Reads a PublicKey from the buffer and advances the cursor position.
+        /// </summary>
+        /// <returns></returns>
+        public U128 ReadU128()
+        {
+            if ((Cursor + 8) > Length) throw new ApplicationException("Buffer exhausted");
+            return new U128(ReadBytes(16));
+        }
+
+    }
+
+}
